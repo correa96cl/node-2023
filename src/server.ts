162b8ab2a805545ccd07d45127a7705135252e7a
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2023-03-15 13:07:28 -0300

    Commit novo

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify';
+import { knex } from './database';
+
+const app = fastify()
+
+app.get('/hello', async () => {
+const test = knex('sqlite_schema').select('*');
+
+return test;
+})
+
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server Running!')
+})
