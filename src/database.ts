e49f29e60c8e8e63e5d0fbffecf76d89dbfbbfc1
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2023-03-20 19:24:25 -0300

    Production configs

--- src/database.ts
+++ src/database.ts
@@ -4,11 +4,12 @@ import { env } from './env';
 
 
 
-export const config: Knex.Config ={
-    client: 'sqlite',
-    connection: {
-        filename: env.DATABASE_URL,
-    },
+export const config: Knex.Config = {
+    client: env.DATABASE_CLIENT,
+    connection:
+        env.DATABASE_CLIENT === 'sqlite' ? {
+            filename: env.DATABASE_URL
+        } : env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
         extension: 'ts',
